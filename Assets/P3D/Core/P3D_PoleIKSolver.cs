dea04f2d027e60c3fc9b2f8d4d132dd63ceb421d
pricedown <josephmisaacs@gmail.com>
2025-12-02 12:27:23 -0500

    Some cleanup of the PoleIKSolver

--- Assets/P3D/Core/P3D_PoleIKSolver.cs
+++ Assets/P3D/Core/P3D_PoleIKSolver.cs
@@ -1,41 +1,39 @@
-using System.Collections;
 using UnityEngine;
-using DG.Tweening;
-using System.Collections.Generic;
 
 namespace pricenerds3D
 {
     /// <summary>
-    /// The PoleIK solver is based on the geometric implementation we completed in class, along with the FABRIK solution.
-    /// You'll be able to switch between both to see the differences!
+    ///     The PoleIK solver is based on the geometric implementation we completed in class, along with the FABRIK solution.
+    ///     You'll be able to switch between both to see the differences!
     /// </summary>
     public class P3D_PoleIKSolver : MonoBehaviour
     {
-        [Header("References")]
-        [SerializeField] 
+        [Header("References")] [SerializeField]
         private P3D_RigDebug _rigInstance; // replace with P3D_RigInstance when ready
         //[SerializeField]
         //private Transform _rigTest2;
 
-        [Header("Pole IK Settings")]
-        [SerializeField] 
+        [Header("Pole IK Settings")] [SerializeField]
         private int _jointsAffected;
-        [SerializeField, Tooltip("The chain creator will start from this joint and work backwards to the parent joints to build the chain")] 
-        private string _jointEndAffected;
+
         [SerializeField]
-        private Transform _endEffectorTarget;
-        [SerializeField] 
-        private Transform _poleTargetEffector;
-        [SerializeField, Range(0, 1)] 
-        private float _weight;
+        [Tooltip(
+            "The chain creator will start from this joint and work backwards to the parent joints to build the chain")]
+        private string _jointEndAffected;
+
+        [SerializeField] private Transform _endEffectorTarget;
+
+        [SerializeField] private Transform _poleTargetEffector;
+
+        [SerializeField] [Range(0, 1)] private float _weight;
+
+        private float angle;
+        private float[] boneLengths;
 
         private P3D_Joint[] jointIKChain;
         private Vector3[] solverPositions;
-        private float[] boneLengths;
         private float targetDistance;
         private float totalChainLength;
-
-        private float angle;
 
         private void Start()
         {
@@ -47,21 +45,30 @@ namespace pricenerds3D
 
             //StartCoroutine(ITest());
         }
-        
+
+        private void Update()
+        {
+            // if (_weight > 0f) FABRIK_SolveIK();
+
+            // Test
+        }
+
         /// <summary>
-        /// This function initializes the chain, starting from the specified end affector and moving up the chain by the specified _jointsAffected
-        /// (FUTURE) - we need some kind of error checking to ensure we dont choose too many bones to affect
+        ///     This function initializes the chain, starting from the specified end affector and moving up the chain by the
+        ///     specified _jointsAffected
+        ///     (FUTURE) - we need some kind of error checking to ensure we dont choose too many bones to affect
         /// </summary>
         private void InitializeChain()
         {
             // initialize chain
             jointIKChain = new P3D_Joint[_jointsAffected];
-            jointIKChain[0] = _rigInstance.rig.m_basePose.m_rig.GetJointFromName(_jointEndAffected); // set end affector first
+            jointIKChain[0] =
+                _rigInstance.rig.m_basePose.m_rig.GetJointFromName(_jointEndAffected); // set end affector first
 
             // add all joints to the chain, working up the hierarchy from the end affector
-            for (int i = 1; i < _jointsAffected; i++)
+            for (var i = 1; i < _jointsAffected; i++)
             {
-                int parentIndex = jointIKChain[i - 1].m_parentIndex;
+                var parentIndex = jointIKChain[i - 1].m_parentIndex;
 
                 // IMPORTANT: not sure if we use base pose here !! may change later
                 jointIKChain[i] = _rigInstance.rig.m_basePose.m_rig.m_joints[parentIndex];
@@ -69,30 +76,26 @@ namespace pricenerds3D
         }
 
         /// <summary>
-        /// Takes the jointIKChain array we initialized and calculates the distance between them
+        ///     Takes the jointIKChain array we initialized and calculates the distance between them
         /// </summary>
         private void CalculateBoneLengths()
         {
-            boneLengths = new float[_jointsAffected];
+            boneLengths = new float[_jointsAffected - 1];
+            totalChainLength = 0.0f;
 
-            for(int i = 0; i < _jointsAffected; i++)
+            for (var i = 0; i < _jointsAffected - 1; i++)
             {
                 // get self to end positions in world space
-                Vector3 start = _rigInstance.rig.m_basePose.m_worldSpace[jointIKChain[i].m_jointIndex].MultiplyPoint3x4(Vector3.zero);
-                Vector3 end = _rigInstance.rig.m_basePose.m_worldSpace[jointIKChain[i].m_parentIndex].MultiplyPoint3x4(Vector3.zero);
+                var start = _rigInstance.rig.m_basePose.m_worldSpace[jointIKChain[i].m_jointIndex]
+                    .MultiplyPoint3x4(Vector3.zero);
+                var end = _rigInstance.rig.m_basePose.m_worldSpace[jointIKChain[i + 1].m_jointIndex]
+                    .MultiplyPoint3x4(Vector3.zero);
 
                 // calculate length
-                float length = Vector3.Distance(start, end);
+                var length = Vector3.Distance(start, end);
                 boneLengths[i] = length;
                 totalChainLength += length;
             }
-        }
-
-        private void Update()
-        {
-            //FABRIK_SolveIK();
-
-            // Test
         }
 
         /*
@@ -131,14 +134,15 @@ namespace pricenerds3D
         }*/
 
         #region FABRIK
+
         private void FABRIK_UpdateBackward()
         {
-
+            // TODO: backward pass (root to effector)
         }
 
         private void FABRIK_UpdateForward()
         {
-
+            // TODO: forward pass (effector to root)
         }
 
         private void FABRIK_SolveIK()
@@ -146,14 +150,20 @@ namespace pricenerds3D
             if (_weight == 0.0f) return;
 
             // get initial world space positions
-            for (int i = 0; i < _jointsAffected; i++)
-            {
-                solverPositions[i] = _rigInstance.rig.m_basePose.m_worldSpace[jointIKChain[i].m_jointIndex].MultiplyPoint3x4(Vector3.zero);
-            }
+            for (var i = 0; i < _jointsAffected; i++)
+                solverPositions[i] = _rigInstance.rig.m_basePose.m_worldSpace[jointIKChain[i].m_jointIndex]
+                    .MultiplyPoint3x4(Vector3.zero);
+
+            var distanceToTarget = Vector3.Distance(solverPositions[_jointsAffected - 1], _endEffectorTarget.position);
+            if (distanceToTarget > totalChainLength)
+                // Target unreachable — stretch toward it
+                // TODO: stretch behavior
+                return;
 
             FABRIK_UpdateForward();
             FABRIK_UpdateBackward();
         }
+
         #endregion
     }
 }
